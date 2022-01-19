f2e90bbeb24b6d87717fca28be5c95c25051b462
sweplaysYT <rasmus.svensk47@gmail.com>
2022-01-19 13:03:45 +0100

    Custom Venyx lib

--- Anime-Fighters-Simulator.lua
+++ Anime-Fighters-Simulator.lua
@@ -1,35 +1,79 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/Venyx-UI-Library/main/source2.lua"))()
 
-local Venyx = library.new("T Hub")
+local Hub = library.new("T Hub")
 
-local MainPage = Venyx:addPage("Main")
-local MainAutoSection MainPage:addSection("Auto")
+local MainPage = Hub:addPage({
+    title = "Main"
+})
 
-MainAutoSection:addDropdown("Auto Train Stat", {"Strength", "Durability", "Chakra", "Sword"}, function(SelectedStat)
-    if SelectedStat == "Strength" then
-        getgenv().TrainStat = "Strength"
-    elseif SelectedStat == "Durability" then
-        getgenv().TrainStat = "Durability"
-    elseif SelectedStat == "Chakra" then
-        getgenv().TrainStat = "Chakra"
-    elseif SelectedStat == "Sword" then
-        getgenv().TrainStat = "Sword"
-    end
-end)
+local MainStatsSection = MainPage:addSection({
+    title = "Stats"
+})
 
-MainAutoSection:addToggle("Toggle Auto Train", nil, function(TrainToggled)
-    if TrainToggled then
-        getgenv().AutoTrain = true
-        while AutoTrain == true do
-            local args = {
-                [1] = "Stat",
-                [2] = TrainStat
-            }
-        
-            game:GetService("ReplicatedStorage").RSPackage.Events.StatFunction:InvokeServer(unpack(args))
-        
+local AutoTrainDropdownContent = {"Strength", "Durability", "Chakra", "Sword"}
+MainStatsSection:addDropdown({
+    title = "Auto Train Stat",
+    list = AutoTrainDropdownContent,
+    callback = function(SelectedStat)
+        if SelectedStat == "Strength" then
+            getgenv().TrainStat = "Strength"
+        elseif SelectedStat == "Durability" then
+            getgenv().TrainStat = "Durability"
+        elseif SelectedStat == "Chakra" then
+            getgenv().TrainStat = "Chakra"
+        elseif SelectedStat == "Sword" then
+            getgenv().TrainStat = "Sword"
         end
-    else
-        getgenv().AutoTrain = false
     end
-end)
+})
+
+MainStatsSection:addSlider({
+    title = "Auto Train Speed",
+    default = 0.05,
+    min = 0.05,
+    max = 2,
+    callback = function(AutoTrainSpeedValue)
+        getgenv().AutoTrainSpeed = AutoTrainSpeedValue
+    end
+})
+
+MainStatsSection:addToggle({
+    title = "Toggle Auto Train",
+    callback = function(AutoTrain)
+        if AutoTrain then
+            getgenv().AutoTrain = true
+            while AutoTrain == true do
+                wait(AutoTrainSpeed)
+                local args = {
+                    [1] = "Stat",
+                    [2] = TrainStat
+                }
+            
+                game:GetService("ReplicatedStorage").RSPackage.Events.StatFunction:InvokeServer(unpack(args))
+            
+            end
+        else
+            getgenv().AutoTrain = false
+        end
+    end
+})
+
+local SettingsPage = Hub:addPage({
+    title = "Settings"
+})
+
+local SettingsKeybindsSection = SettingsPage:addSection({
+    title = "Keybinds"
+})
+
+SettingsKeybindsSection:addKeybind({
+    title = "Toggle Menu Key (Standard: Home)",
+    key = Enum.KeyCode.Insert,
+    callback = function()
+        print("Activated Keybind!")
+        Hub:toggle()
+    end,
+    changedCallback = function(key)
+        print("Changed Keybind to", key)
+    end
+})
